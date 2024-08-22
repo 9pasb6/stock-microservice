376e1b0e28d6f72036c8a57b0a4ca40b4225aa7b
Pablo Bernal <pasb@MacBook-Air-de-Pablo-Bernal.local>
2024-08-22 14:27:30 -0500

    first commit

---
+++ src/test/java/emazon/microservice/stock_microservice/StockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package emazon.microservice.stock_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
